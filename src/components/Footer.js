c4d44121800d15ba2ed2738a9ccbd9b69521bc31
Pankaj Kumar Dhal <pk99dhal@gmail.com>
2025-01-07 12:59:23 +0530

    Header & Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gp-30 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control py-1"
+                  placeholder="Year Email Address.."
+                  aria-label="Year Email Address.."
+                  aria-describedby="basic-addon2"
+                />
+                <span class="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <h4 className="text-white mb-4">Contact</h4>
+              <div>
+                <address className="text-white fs-6">Hno : Bangalore, HSR LAyout <br />6th crossbr
+                PinCode:560040</address>
+                <a href="tel:+9108917237614" className="mt-3 d-block mb-1 text-white">
+                  +91 8917237614
+                </a>
+                <a href="tel:+9108917237614" className="mt-3 d-block mb-0 text-white">
+                  pankaj21dhal@gmail.com
+                </a>
+                <div className="social-icons d-flex align-items-center gap-30 mt-4">
+                  <a className="text-white" href="">
+                    <FaLinkedin className="fs-3"/>
+                  </a>
+                  <a className="text-white" href="">
+                    <FaGithub className="fs-3"/>
+                  </a>
+                  <a className="text-white" href="">
+                    <FaInstagram className="fs-3"/>
+                  </a>
+                  <a className="text-white" href="">
+                  <FaSquareXTwitter  className="fs-3"/>
+                  </a>
+                </div>
+              </div>
+              
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refond Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Term & Conditions</Link>
+
+              </div>
+            </div>
+            <div className="col-4">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">About us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watchs</Link>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Powered by Developer,s Corner
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
